97003fff5d16c5e798ec00985c6c9672e1966c0d
unknown <justindumouchelle@gmail.com>
2021-03-24 13:27:33 -0400

    fix for scip test

--- recipe/run_scip_test.bat
+++ recipe/run_scip_test.bat
@@ -1,6 +1,10 @@
 cmake -G"NMake Makefiles" -B build -S scipoptsuite/scip/examples/Queens -D CMAKE_BUILD_TYPE=Release
+if errorlevel 1 exit 1
 
 cmake --build build --parallel ${CPU_COUNT}
+if errorlevel 1 exit 1
 
 .\build\queens 5
+if errorlevel 1 exit 1
 scip --version
+if errorlevel 1 exit 1
