4bd31ad272c95626d02674719fe5afd9aebd042d
unknown <justindumouchelle@gmail.com>
2021-03-24 12:31:51 -0400

    fix for bld.bat

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -1,21 +1,12 @@
-"%PYTHON%" setup.py install
-if errorlevel 1 exit 1
-
-set -o errexit
-if errorlevel 1 exit 1
-set -o pipefail
-if errorlevel 1 exit 1
-set -o nounset
-if errorlevel 1 exit 1
-
-cmake -B bliss-build -S "%SRC_DIR%\bliss" -D CMAKE_BUILD_TYPE=Release -D BUILD_SHARED_LIBS=OFF
+cmake -G"NMake Makefiles" -B bliss-build -S "%SRC_DIR%\bliss" -D CMAKE_BUILD_TYPE=Release -D BUILD_SHARED_LIBS=OFF
 if errorlevel 1 exit 1
 cmake --build bliss-build --parallel %CPU_COUNT%
 if errorlevel 1 exit 1
-cmake --install bliss-build --prefix "%PWD%\bliss-install"
+cmake --install bliss-build --prefix "bliss-install"
 if errorlevel 1 exit 1
 
-cmake -B scipoptsuite-build -S "%SRC_DIR%\scipoptsuite" ^
+cmake -G"NMake Makefiles" ^
+      -B scipoptsuite-build -S "%SRC_DIR%\scipoptsuite" ^
       -D CMAKE_BUILD_TYPE=Release ^
       -D PARASCIP=ON ^
       -D PAPILO=ON ^
@@ -23,16 +14,12 @@ cmake -B scipoptsuite-build -S "%SRC_DIR%\scipoptsuite" ^
       -D GCG=ON ^
       -D ZIMPL=ON ^
       -D BOOST=ON ^
-      -D GMP=ON ^
-      -D QUADMATH=ON ^
       -D IPOPT=ON ^
       -D ZLIB=ON ^
-      -D READLINE=ON ^
       -D SYM=bliss ^
-      -D BLISS_DIR="%PWD%\bliss-install" ^
-      -D EXPRINT=cppad ^
-      -D GSL=ON ^
-      -D CLIQUER=ON
+      -D BLISS_DIR="bliss-install" ^
+      -D EXPRINT=cppad
+      
 if errorlevel 1 exit 1
 cmake --build scipoptsuite-build --parallel %CPU_COUNT%
 if errorlevel 1 exit 1
