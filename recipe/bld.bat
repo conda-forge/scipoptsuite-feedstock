2e1d91ad660398325deb36f58779343ff30b09e5
AntoinePrv <AntoinePrv@users.noreply.github.com>
2021-03-29 16:42:38 -0400

    Formatting

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -1,4 +1,8 @@
-cmake -G"NMake Makefiles" -B bliss-build -S "%SRC_DIR%\bliss" -D CMAKE_BUILD_TYPE=Release -D BUILD_SHARED_LIBS=OFF
+cmake -G"NMake Makefiles"  ^
+      -B bliss-build ^
+      -S "%SRC_DIR%\bliss" ^
+      -D CMAKE_BUILD_TYPE=Release ^
+      -D BUILD_SHARED_LIBS=OFF
 if errorlevel 1 exit 1
 cmake --build bliss-build --parallel %CPU_COUNT%
 if errorlevel 1 exit 1
@@ -6,7 +10,8 @@ cmake --install bliss-build --prefix "bliss-install"
 if errorlevel 1 exit 1
 
 cmake -G"NMake Makefiles" ^
-      -B scipoptsuite-build -S "%SRC_DIR%\scipoptsuite" ^
+      -B scipoptsuite-build ^
+      -S "%SRC_DIR%\scipoptsuite" ^
       -D CMAKE_BUILD_TYPE=Release ^
       -D PARASCIP=ON ^
       -D PAPILO=ON ^
@@ -19,7 +24,6 @@ cmake -G"NMake Makefiles" ^
       -D SYM=bliss ^
       -D BLISS_DIR="bliss-install" ^
       -D EXPRINT=cppad
-      
 if errorlevel 1 exit 1
 cmake --build scipoptsuite-build --parallel %CPU_COUNT%
 if errorlevel 1 exit 1
