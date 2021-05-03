5b0a7f92a329397c36234e9affedeb5313f309df
AntoinePrv <AntoinePrv@users.noreply.github.com>
2021-05-03 14:34:06 -0400

    Properly add IPOPT on MacOS

--- recipe/run_scip_test.sh
+++ recipe/run_scip_test.sh
@@ -9,4 +9,8 @@ cmake -B build -S scipoptsuite/scip/examples/Queens -D CMAKE_BUILD_TYPE=Release
 cmake --build build --parallel ${CPU_COUNT}
 
 ./build/queens 5
+
 scip --version
+
+# Verifies that IPOPT is properly linked
+scip --version | grep -E 'Ipopt\s+[0-9]+\.[0-9]+\.[0-9]+'
