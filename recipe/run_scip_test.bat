f7ecc9f8e84dccae381599643552b7b27ee32496
Antoine Prouvost <AntoinePrv@users.noreply.github.com>
2021-05-10 19:05:59 -0400

    Change Select-String to findstr

--- recipe/run_scip_test.bat
+++ recipe/run_scip_test.bat
@@ -8,5 +8,5 @@ if errorlevel 1 exit 1
 if errorlevel 1 exit 1
 scip --version
 if errorlevel 1 exit 1
-scip --version | Select-String -Pattern 'Ipopt\s+[0-9]+\.[0-9]+\.[0-9]+' -CaseSensitive
+scip --version | findstr /r /c:"Ipopt[ ]*[0-9][0-9]*\.[0-9][0-9]*\.[0-9][0-9]*"
 if errorlevel 1 exit 1
