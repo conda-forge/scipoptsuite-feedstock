72b85965ca13019d13dc133e6e3aae0e06ba6942
AntoinePrv <AntoinePrv@users.noreply.github.com>
2021-12-15 12:18:16 +0100

    Patch FindIPOPT.cmake

--- recipe/run_scip_test.bat
+++ recipe/run_scip_test.bat
@@ -6,5 +6,10 @@ if errorlevel 1 exit 1
 
 .\build\queens 5
 if errorlevel 1 exit 1
+
 scip --version
+if errorlevel 1 exit 1
+
+REM Verifies that IPOPT is properly linked
+scip --version | findstr /r /c:"Ipopt[ ]*[0-9][0-9]*\.[0-9][0-9]*\.[0-9][0-9]*"
 if errorlevel 1 exit 1
