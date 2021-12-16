9638e42d3e1584913b26117789afd1fecc39687b
AntoinePrv <AntoinePrv@users.noreply.github.com>
2021-12-16 09:45:33 +0100

    Improve SCIP tests

--- recipe/run_scip_test.sh
+++ recipe/run_scip_test.sh
@@ -5,12 +5,14 @@ set -o pipefail
 set -o nounset
 
 cmake -B build -S scipoptsuite/scip/examples/Queens -D CMAKE_BUILD_TYPE=Release
-
 cmake --build build --parallel ${CPU_COUNT}
-
 ./build/queens 5
 
 scip --version
 
-# Verifies that IPOPT is properly linked
-scip --version | grep -E 'Ipopt\s+[0-9]+\.[0-9]+\.[0-9]+'
+# Verifies that dependencies are properly linked
+scip --version | grep -iE 'CppAD\s+[0-9]+'
+scip --version | grep -iE 'ZLIB\s+[0-9]+\.[0-9]+\.[0-9]+'
+scip --version | grep -iE 'GMP\s+[0-9]+\.[0-9]+\.[0-9]+'
+scip --version | grep -iE 'bliss\s+[0-9]+\.[0-9]+\.[0-9]+'
+scip --version | grep -iE 'Ipopt\s+[0-9]+\.[0-9]+\.[0-9]+'
