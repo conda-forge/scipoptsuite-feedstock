384d80ff37e3db107228efc4ae2b01a38a6435a6
AntoinePrv <AntoinePrv@users.noreply.github.com>
2023-11-20 16:42:45 +0100

    Try MACOSX target

--- recipe/run_soplex_test.sh
+++ recipe/run_soplex_test.sh
@@ -5,7 +5,6 @@ set -o pipefail
 set -o nounset
 
 export CXXFLAGS="${CXXFLAGS} -D_LIBCPP_DISABLE_AVAILABILITY"
-export MACOSX_DEPLOYMENT_TARGET="10.13"
 
 cat > CMakeLists.txt << 'EOF'
 cmake_minimum_required(VERSION 3.0)
